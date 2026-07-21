b1e00f987ea829d7ea4bc32ee018e6636e49cedb
Ilya <147517764+invdevv@users.noreply.github.com>
2026-07-21 12:54:18 +0500

    a

--- backend/src/subscriptions/router.py
+++ backend/src/subscriptions/router.py
@@ -6,4 +6,4 @@ router = APIRouter(prefix="/sub", tags=["subscriptions"])
 @router.get("/{short_uuid}")
 async def get(short_uuid: str):
     sub = await Subscriptions.get(short_uuid)
-    return Response(content=sub, headers={""})
+    return Response(content=sub)
